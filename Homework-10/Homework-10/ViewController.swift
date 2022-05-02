8a89cd04439b344438defa8b8b83a1855e708f14
Andrik <masmay.pl@gmail.com>
2022-05-02 22:54:36 +0200

    initial commit

---
+++ Homework-10/Homework-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-10
+//
+//  Created by Andrei Maskal on 02/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
